4e04968bc835c5bc72e26ca66a1b9160fe6bf9b2
gustingurahagung <igustingurah.f52121039@gmail.com>
2023-11-26 15:36:31 +0800

    UserAcoountScreen

--- app/src/main/java/academy/bangkit/trackmate/view/app/account/UserAccountScreen.kt
+++ app/src/main/java/academy/bangkit/trackmate/view/app/account/UserAccountScreen.kt
@@ -1,28 +1,203 @@
 package academy.bangkit.trackmate.view.app.account
 
-import androidx.compose.foundation.layout.Box
+import academy.bangkit.trackmate.R
+import academy.bangkit.trackmate.ui.theme.TrackMateTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun UserAccountScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = Modifier.align(Alignment.Center)
+        ConstraintLayout {
+            val (topImg, profile) = createRefs()
+            Image(painterResource(id = R.drawable.top_background), null,
+                Modifier
+                    .fillMaxWidth()
+                    .constrainAs(topImg) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    })
+            Image(painterResource(id = R.drawable.profile), null,
+                Modifier
+
+                    .constrainAs(profile) {
+                        top.linkTo(topImg.bottom)
+                        bottom.linkTo(topImg.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    })
+        }
+        Text(
+            "Ngurah Agung",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 16.dp),
+            color = Color.Black
+        )
+        Text(
+            "ngurahagung@gmail.com",
+            fontSize = 20.sp,
+            color = Color.Black
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 10.dp, bottom = 10.dp)
+                .height(55.dp)
+            , shape = RoundedCornerShape(15)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_1),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(end = 5.dp)
+                        .clickable { }
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "My Reviews",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 10.dp, bottom = 10.dp)
+                .height(55.dp),
+            shape = RoundedCornerShape(15)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_2),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(end = 5.dp)
+                        .clickable { }
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Edit Profile",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 10.dp, bottom = 10.dp)
+                .height(55.dp), shape = RoundedCornerShape(15)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_3),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(end = 5.dp)
+                        .clickable { }
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Personal Information",
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        Button(
+            onClick = { /*TODO: Implement logout functionality*/ },
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 100.dp, bottom = 10.dp)
+                .height(55.dp)
+            , shape = RoundedCornerShape(15)
         ) {
             Text(
-                text = "User Account Screen",
-                textAlign = TextAlign.Center,
-                modifier = Modifier.align(Alignment.CenterHorizontally)
+                text = "Log Out",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AccountPreview() {
+    TrackMateTheme {
+        Surface {
+            UserAccountScreen(navController = rememberNavController())
         }
     }
 }
